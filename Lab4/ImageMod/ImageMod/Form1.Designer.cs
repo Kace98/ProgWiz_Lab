a720546bdc1c131d09005f91db058a7edf419108
Kace98 <grzegorz.kacprzynski@student.put.poznan.pl>
2025-03-27 12:18:42 +0100

    Pierwsza faza lab4

---
+++ Lab4/ImageMod/ImageMod/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ImageMod
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button_load = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button_load
+            // 
+            button_load.Location = new Point(12, 396);
+            button_load.Name = "button_load";
+            button_load.Size = new Size(121, 36);
+            button_load.TabIndex = 0;
+            button_load.Text = "Load";
+            button_load.UseVisualStyleBackColor = true;
+            button_load.Click += button_load_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Location = new Point(149, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(420, 420);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(581, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(button_load);
+            Name = "Form1";
+            Text = "Edytor zdjęć";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button_load;
+        private PictureBox pictureBox1;
+    }
+}
