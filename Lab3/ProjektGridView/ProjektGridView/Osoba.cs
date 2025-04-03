6c6e734b6783eca5c634e0f5b14b495a9b116bd4
Kace98 <grzegorz.kacprzynski@student.put.poznan.pl>
2025-04-03 12:54:08 +0200

    Dodano Osoba.cs z klasą Osoba do późniejszego wykorzystania

---
+++ Lab3/ProjektGridView/ProjektGridView/Osoba.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ProjektGridView
+{
+    class Osoba
+    {
+        public int Id { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public int Wiek { get; set; }
+        public string Stanowisko { get; set; }
+    }
+}
